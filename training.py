f3799211b588ecf8e5fa5466ffb27693c05c9cb8
utkarsh030201 <53508181+utkarsh030201@users.noreply.github.com>
2021-12-17 16:09:10 +0530

    Update training.py

--- training.py
+++ training.py
@@ -1,6 +1,6 @@
 from utils import *
 from sklearn.model_selection import train_test_split
-
+import matplotlib.pyplot as plt
 path = 'myData'
 data = importDataInfo(path)
 
@@ -29,10 +29,10 @@ history = model.fit(batch.Gen(xTrain,yTrain,10,1),steps_per_epoch=300,epochs=10,
 model.save('model.h5')
 print('Model Saved')
 
-plt.plot(history.history['loss']
-plt.plot(history.history['val_loss']
-plt.legend(['Training', 'Validation')
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.legend(['Training', 'Validation'])
 plt.ylim([0,0.2])
-plt.title('Loss)'
+plt.title('Loss')
 plt.xlabel('Epoch')
 plt.show
